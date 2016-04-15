a67534856368390ecfde01aff1ad7b1b698fa131
Kevin Phelps <KevinPhelps11@gmail.com>
2016-04-15 02:21:19 -0500

    automatically order typescript sources based on explicit references

--- index.js
+++ index.js
@@ -3,14 +3,25 @@
 let fs = require('fs');
 let typescript = require('typescript');
 
-const tsFiles = [
-    './implementation/utility.ts',
-    './implementation/dto-generator.ts',
-    './implementation/index.ts'
-];
-
-let tsCode = tsFiles
-    .map(file => fs.readFileSync(file).toString())
+let tsCode = getDependenciesAndSelf('./implementation/index.ts')
+    .map(filePath => fs.readFileSync(filePath).toString())
     .join('\n');
 
 eval(typescript.transpile(tsCode));
+
+function getDependenciesAndSelf(path) {
+    let references = [];
+    
+    let file = fs.readFileSync(path).toString();
+    let folder = path.substring(0, path.lastIndexOf('/'));
+    
+    let referenceMatch;
+    let referenceRegex = /\/\/\/ <reference path="([a-z-/]+(?:.d)?.ts)" \/>/g;
+    while (!!(referenceMatch = referenceRegex.exec(file))) {
+        let reference = referenceMatch[1];
+        references = getDependenciesAndSelf(`${folder}/${reference}`).concat(references);
+    }
+    
+    references.push(path);
+    return references;
+}
