01dd815c4d544f4f97854a0bee4b633848c2c8e9
Kevin Phelps <KevinPhelps11@gmail.com>
2016-04-15 02:29:34 -0500

    tweak to reference follow code

--- index.js
+++ index.js
@@ -3,25 +3,24 @@
 let fs = require('fs');
 let typescript = require('typescript');
 
-let tsCode = getDependenciesAndSelf('./implementation/index.ts')
+let tsCode = getReferencesAndSelf('./implementation/index.ts')
     .map(filePath => fs.readFileSync(filePath).toString())
     .join('\n');
 
 eval(typescript.transpile(tsCode));
 
-function getDependenciesAndSelf(path) {
-    let references = [];
+function getReferencesAndSelf(filePath) {
+    let references = [filePath];
     
-    let file = fs.readFileSync(path).toString();
-    let folder = path.substring(0, path.lastIndexOf('/'));
+    let file = fs.readFileSync(filePath).toString();
+    let folder = filePath.substring(0, filePath.lastIndexOf('/'));
     
     let referenceMatch;
     let referenceRegex = /\/\/\/ <reference path="([a-z-/]+(?:.d)?.ts)" \/>/g;
     while (!!(referenceMatch = referenceRegex.exec(file))) {
         let reference = referenceMatch[1];
-        references = getDependenciesAndSelf(`${folder}/${reference}`).concat(references);
+        references = getReferencesAndSelf(`${folder}/${reference}`).concat(references);
     }
     
-    references.push(path);
     return references;
 }
