0563393d837704c42986217dca7323efd4d1bd15
Kevin Phelps <KevinPhelps11@gmail.com>
2016-04-15 13:39:48 -0500

    use my ts-runner module

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 'use strict';
 
-let fs = require('fs');
-let typescript = require('typescript');
-
-let fileContentsCache = {};
-
-let tsSources = getReferencesAndSelf('./implementation/index.ts');
-
-let tsCode = tsSources
-    .map(filePath => fileContentsCache[filePath])
-    .join('\n');
-
-eval(typescript.transpile(tsCode));
-
-function getReferencesAndSelf(filePath) {
-    let references = [filePath];
-    
-    let folder = filePath.substring(0, filePath.lastIndexOf('/'));
-    
-    let fileContents = fs.readFileSync(filePath).toString();
-    fileContentsCache[filePath] = fileContents;
-    
-    let referenceMatch;
-    let referenceRegex = /\/\/\/ <reference path="([a-z-/]+(?:.d)?.ts)" \/>/g;
-    while (!!(referenceMatch = referenceRegex.exec(fileContents))) {
-        let reference = referenceMatch[1];
-        references = getReferencesAndSelf(`${folder}/${reference}`).concat(references);
-    }
-    
-    return references.filter((value, index, self) => self.indexOf(value) === index);
-}
+let tsRunner = require('ts-runner');
+tsRunner.run('./implementation/index.ts', this, arguments);
