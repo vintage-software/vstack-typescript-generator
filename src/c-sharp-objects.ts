40d1a5fca3ce8371ae396f5520ced5b996333952
Kevin Phelps <KevinPhelps11@gmail.com>
2017-10-19 09:02:51 -0500

    refactor: make csharp object properties readonly

--- src/c-sharp-objects.ts
+++ src/c-sharp-objects.ts
@@ -1,48 +1,77 @@
 export class CSharpType {
-  constructor(public namespace: string, public name: string, public inherits: string[]) {
-  }
+  constructor(
+    public readonly namespace: string,
+    public readonly name: string,
+    public readonly inherits: string[]
+  ) { }
 }
 
 export class CSharpClassOrStructOrInterface extends CSharpType {
-  constructor(public namespace: string, public name: string, public inherits: string[], public constructors: CSharpContructor[], public properties: CSharpProperty[]) {
+  constructor(
+    public readonly namespace: string,
+    public readonly name: string,
+    public readonly inherits: string[],
+    public readonly constructors: CSharpContructor[],
+    public readonly properties: CSharpProperty[]
+  ) {
     super(namespace, name, inherits);
   }
 }
 
 export class CSharpMemberType {
-  constructor(public name: string, public isNullable: boolean, public isCollection: boolean) {
-  }
+  constructor(
+    public readonly name: string,
+    public readonly isNullable: boolean,
+    public readonly isCollection: boolean
+  ) { }
 }
 
 export class CSharpContructor {
-  constructor(public parameters: CSharpParameter[]) {
-  }
+  constructor(
+    public readonly parameters: CSharpParameter[]
+  ) { }
 }
 
 export class CSharpMember {
-  constructor(public type: CSharpMemberType, public name: string) {
-  }
+  constructor(
+    public readonly type: CSharpMemberType,
+    public readonly name: string
+  ) { }
 }
 
 export class CSharpProperty extends CSharpMember {
-  constructor(public type: CSharpMemberType, public name: string) {
+  constructor(
+    public readonly type: CSharpMemberType,
+    public readonly name: string
+  ) {
     super(type, name);
   }
 }
 
 export class CSharpParameter extends CSharpMember {
-  constructor(public type: CSharpMemberType, public name: string, public defaultValue: string) {
+  constructor(
+    public type: CSharpMemberType,
+    public name: string,
+    public defaultValue: string
+  ) {
     super(type, name);
   }
 }
 
 export class CSharpEnum extends CSharpType {
-  constructor(public namespace: string, public name: string, public inherits: string[], public entries: CSharpEnumEntry[]) {
+  constructor(
+    public namespace: string,
+    public name: string,
+    public inherits: string[],
+    public entries: CSharpEnumEntry[]
+  ) {
     super(namespace, name, inherits);
   }
 }
 
 export class CSharpEnumEntry {
-  constructor(public name: string, public value: number) {
-  }
+  constructor(
+    public name: string,
+    public value: number
+  ) { }
 }
