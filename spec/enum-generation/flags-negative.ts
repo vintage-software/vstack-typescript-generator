3fadc8eaf5aa99d500cfb58c5bb1bc9f4ddb77cd
Kevin Phelps <KevinPhelps11@gmail.com>
2018-01-01 19:10:59 -0600

    fix (enum): support negative and inverted values

---
+++ spec/enum-generation/flags-negative.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+import 'jasmine';
+import { tsGenerator } from '../../src/tsgen';
+
+const sampleFile = `using System;
+
+namespace MyNamespace.Domain
+{
+  public enum Colors
+  {
+    None = 0
+    Red = 1,
+    Orange = 2,
+    Yellow = 4,
+    Green = 8,
+    Blue = 16,
+    Indigo = 32,
+    Violet = 64,
+    All = -1
+  }
+}`;
+
+const expectedOutput = `export enum Colors {
+  None = 0,
+  Red = 1,
+  Orange = 2,
+  Yellow = 4,
+  Green = 8,
+  Blue = 16,
+  Indigo = 32,
+  Violet = 64,
+  All = -1
+}`;
+
+describe('vstack-typescript-generation enum generator', () => {
+  it('should transform an enum with negative values correctly', () => {
+    const result = tsGenerator(sampleFile);
+    expect(result).toEqual(expectedOutput);
+  });
+});
--- src/generate-enum.ts
+++ src/generate-enum.ts
@@ -1,4 +1,4 @@
-import { CSharpEnum } from './c-sharp-objects';
+import { CSharpEnum, CSharpEnumEntry } from './c-sharp-objects';
 
 export function generateEnum(cSharpEnum: CSharpEnum): string {
   'use strict';
@@ -6,10 +6,33 @@ export function generateEnum(cSharpEnum: CSharpEnum): string {
   let nextIndex = 0;
   const entryStrings: string[] = [];
   for (const entry of cSharpEnum.entries) {
-    entryStrings.push(`${entry.name} = ${entry.value || nextIndex}`);
+    const value = !!entry.value ? parseEnumValue(entry.value, cSharpEnum.entries) : undefined;
 
-    nextIndex = isNaN(entry.value) ? nextIndex + 1 : entry.value + 1;
+    entryStrings.push(`${entry.name} = ${value !== undefined ? value : nextIndex}`);
+
+    nextIndex = isNaN(value) ? nextIndex + 1 : value + 1;
   }
 
   return `export enum ${cSharpEnum.name} {\n  ${entryStrings.join(',\n  ')}\n}`;
+}
+
+function parseEnumValue(value: string, entries: CSharpEnumEntry[]): number {
+  let parsedValue: number;
+
+  if (value.startsWith('~')) {
+    const valueNoTilde = value.substr(1);
+    const matchingEntry = entries.find(e => e.name === valueNoTilde);
+
+    // tslint:disable-next-line:no-bitwise
+    parsedValue = matchingEntry ? ~parseEnumValue(matchingEntry.value, entries) : ~parseAsDecimalOrHexadecimal(valueNoTilde);
+  } else {
+    parsedValue = parseAsDecimalOrHexadecimal(value);
+  }
+
+  return parsedValue;
+}
+
+function parseAsDecimalOrHexadecimal(value: string) {
+  const radix = value.startsWith('0x') ? 16 : 10;
+  return parseInt(value, radix);
 }
