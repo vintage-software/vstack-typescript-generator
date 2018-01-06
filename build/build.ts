2ae3cd7a92174ff309640ace2e1ae4a202cbb059
Kevin Phelps <KevinPhelps11@gmail.com>
2018-01-06 05:04:23 -0600

    chore: update build script to run test script

--- build/build.ts
+++ build/build.ts
@@ -31,18 +31,6 @@ bailIf(options.watch && options.test, '--watch and --test are mutually exclusive
   await execute(`tsc --project ./tsconfig.json ${options.watch ? '--watch' : ''}`);
 
   if (options.test) {
-    await execute(`tsc --project ./tsconfig.spec.json`);
-    await execute('istanbul cover ./node_modules/jasmine/bin/jasmine.js --dir ./coverage/unit --print none -- --config=jasmine.json');
-    await e2e();
-    await execute('remap-istanbul -i ./coverage/unit/coverage.json -o ./coverage/unit/coverage.json -t json');
-    await execute('remap-istanbul -i ./coverage/e2e/coverage.json -o ./coverage/e2e/coverage.json -t json');
-    await execute('istanbul report -t lcov');
-    await execute('istanbul report -t text-summary');
-    await execute('istanbul check-coverage --statements 90 --branches 90 --functions 90 --lines 90');
+    await execute('ts-node ./build/test.ts');
   }
 })();
-
-async function e2e() {
-  await execute('istanbul cover ./dist-spec/src/tsgen-cli.js --dir ./coverage/e2e --print none -- ./test-project/tsgen.json');
-  await execute('dircompare -c ./test-project/generated ./test-project/test-generated');
-}
